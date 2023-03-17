2c45cda194cd1c6589552dae6f4341def5964546
manuel <mawilespinosa@gmail.com>
2023-03-17 17:12:30 -0500

    creacion de tabla con js

--- funsiones.js
+++ funsiones.js
@@ -5,3 +5,9 @@ function doGet() {
 function obtenerDatosHtml(nombre) {        
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+
+function obtenerContactos() {
+    let hoja = SpreadsheetApp.openById('18YVSU3POoSjmWfUIPBYArjTG2U5i1b0E1uB62uBpLho').getActiveSheet();
+    let datos = hoja.getDataRange().getValues();
+    return datos
+}
