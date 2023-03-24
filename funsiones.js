e5c882600c2c687f9c3a4ef5905ec9b4bd0fee96
manuel <mawilespinosa@gmail.com>
2023-03-23 19:59:18 -0500

    Se creo el metodo post, y se insertaron datos en el archivo con el metodo dialog y onsubmit en el form

--- funsiones.js
+++ funsiones.js
@@ -1,13 +1,20 @@
+const HOJA = SpreadsheetApp.openById('18YVSU3POoSjmWfUIPBYArjTG2U5i1b0E1uB62uBpLho').getActiveSheet();
+
 function doGet() {
-  return HtmlService.createTemplateFromFile('web').evaluate();
+  return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda de Contactos');
 }
 
 function obtenerDatosHtml(nombre) {        
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
 
-function obtenerContactos() {
-    let hoja = SpreadsheetApp.openById('18YVSU3POoSjmWfUIPBYArjTG2U5i1b0E1uB62uBpLho').getActiveSheet();
-    let datos = hoja.getDataRange().getValues();
-    return datos
+function obtenerContactos() {    
+    return HOJA.getDataRange().getValues();    
+}
+
+function doPost() {  
+  return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda de Contactos');
+}
+function insertarContacto(nombre,correo) {
+  HOJA.appendRow([nombre,correo])
 }
