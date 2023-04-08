da11a5987d547df97abd578a5433374af15f57ea
manuel <mawilespinosa@gmail.com>
2023-04-08 13:06:30 -0500

    se abre explorador de archivos para subir imagen y se guardan en google drive.

--- funciones.js
+++ funciones.js
@@ -1,4 +1,6 @@
 const HOJA = SpreadsheetApp.openById('18YVSU3POoSjmWfUIPBYArjTG2U5i1b0E1uB62uBpLho').getActiveSheet();
+const CARPETA = DriveApp.getFolderById('1afQvMBn9Cxd2r-FFP3-TkR7Dg-RHnuCP');
+const urlAccesoDrive = 'https://drive.google.com/uc?export=view&id=';
 
 function doGet() {
   return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda de Contactos');
@@ -15,8 +17,13 @@ function obtenerContactos() {
 function doPost() {
   return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda de Contactos');
 }
-function insertarContacto(nombre, apellido, correo, telefono) {
-  HOJA.appendRow([nombre, apellido, correo, telefono])
+function insertarContacto(contacto,imagen) {
+  if(imagen){
+    let blob = Utilities.newBlob(imagen.datos,imagen.tipo,imagen.nombre);
+    let archivo = CARPETA.createFile(blob);
+    contacto.imagen = urlAccesoDrive+archivo.getId();
+  }
+  HOJA.appendRow([contacto.nombre, contacto.apellido, contacto.correo, contacto.tel,contacto.imagen]);
 }
 function eliminarContacto(numFila) {
   HOJA.deleteRow(numFila);
